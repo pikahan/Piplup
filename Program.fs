207088afd87d34f4a1143564df12afee21718e9b
pikahan <yhl990104@gmail.com>
2019-11-18 08:35:11 +0800

    update: update config

--- Program.fs
+++ Program.fs
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Parse
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    fromString argv.[0]
+    // printfn "Hello World from F#! %A" argv.[0]
     0 // return an integer exit code
